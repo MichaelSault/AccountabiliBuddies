b2b5a0a9e6c5b8cc1b7346d48e2ce7946b38d59d
MichaelSault <michael_sault@hotmail.com>
2023-06-11 22:38:23 -0400

    initial commit

---
+++ accountabilibuddies/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Welcome to AccountibiliBuddies
+        </h2>
+          (Yes we know that's a mouthfull)
+      </header>
+    </div>
+  );
+}
+
+export default App;
