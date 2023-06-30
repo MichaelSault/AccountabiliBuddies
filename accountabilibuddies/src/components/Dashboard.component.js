2f8094fd3efe2cb3fdc1453df9e274046d8e2fd3
MichaelSault <michael_sault@hotmail.com>
2023-06-29 23:21:54 -0400

    layout changes with grid/flexbox

--- accountabilibuddies/src/components/Dashboard.component.js
+++ accountabilibuddies/src/components/Dashboard.component.js
@@ -1,13 +1,24 @@
 import MainCard from './MainCard.component';
+import Grid from '@mui/material/Grid';
+
 import '../App.css';
 
 
 export default function Dashboard() {
     return(
         <>
-            <MainCard title="100 Days of Code" secondary="daily" background="#006699" width="200px"/>
-            <MainCard title="Read Before Bed" secondary="4x weekly" background="#663366"/>
-            <MainCard title="Get 8 hours of sleep" secondary="daily" background="firebrick"/>
+            <Grid container spacing={0.5}>
+                <Grid item xs={4}>
+                    <MainCard title="100 Days of Code" secondary="daily" background="#006699" width="200px"/>
+                </Grid>
+                <Grid item xs={4}>
+                    <MainCard title="Read Before Bed" secondary="4x weekly" background="#663366"/>
+                </Grid>
+                <Grid item xs={4}>
+                    <MainCard title="Get 8 hours of sleep" secondary="daily" background="firebrick"/>
+                </Grid>
+            </Grid>
+            
             <MainCard title="Mow the Lawn" secondary="weekly" background="darkgreen"/>
         </>
     );
